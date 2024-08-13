2811d7c340c29156999cab4d6052160a59bc20b9
Hvbris <44342309+hvbr1s@users.noreply.github.com>
2024-08-13 15:01:13 +0200

    Update call.py

--- llm/call.py
+++ llm/call.py
@@ -3,8 +3,8 @@ import math
 import json
 from dotenv import load_dotenv
 from openai import AsyncOpenAI
-from system.prompt_sol import preprare_sol_prompt
-from system.prompt_evm import preprare_evm_prompt
+from system.prompt_sol import prepare_sol_prompt
+from system.prompt_evm import prepare_evm_prompt
 
 # Load secrets
 load_dotenv()
@@ -15,19 +15,24 @@ openai_client = AsyncOpenAI(api_key=openai_key)
 openai_model_prod = "gpt-4o-2024-08-06"
 
 # Function to run the bot on a file and get the complexity score
-async def get_complexity_score(file_path, file_info):
+async def get_complexity_score(file_path, file_info, chain):
     try:
         rust_program = file_info['file_content']
         code_lines = str(file_info['code_lines'])
         comment_lines = str(file_info['comment_lines'])
         # Compute code to comment ratio
         code_to_comment_ratio = math.ceil((int(comment_lines) / int(code_lines)) * 100)
+        # Prepare system prompt based on chain
+        if chain == "sol":
+            system_prompt = await prepare_sol_prompt(file_path, code_lines , file_info['comment_lines'], code_to_comment_ratio)
+        elif chain == "evm":
+            system_prompt= await prepare_evm_prompt(file_path, code_lines , file_info['comment_lines'], code_to_comment_ratio)
 
         response = await openai_client.chat.completions.create(
             temperature=0.0,
             model=openai_model_prod,
             messages=[
-                {"role": "system", "content": await preprare_sol_prompt(file_path, code_lines , file_info['comment_lines'], code_to_comment_ratio)},
+                {"role": "system", "content": system_prompt},
                 {"role": "user", "content": rust_program}
             ],
             response_format= { "type": "json_object" },
